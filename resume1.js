5680d13378cccb7f99d3cea8e78e84f0d3b12c64
Lovepreet-G <96777119+Lovepreet-G@users.noreply.github.com>
2023-07-07 16:52:17 +0530

    Resume

---
+++ resume1.js
@@ -0,0 +1,50 @@
+// var aboutid = document.getElementById("about");
+// var skillid = document.getElementById("skills");
+// var experienceid = document.getElementById("experience");
+// var portfolioid = document.getElementById("portfolio");
+// var contactid = document.getElementById("contant");
+
+
+
+// function scroll()
+// {
+//     let coord = aboutid.getBoundingClientRect();
+//     if(coord.top <= 0)
+//     {
+//         clearInterval
+//     }
+
+// }
+
+
+
+
+var navManu =document.querySelectorAll('.nav-menu a');
+var interval;
+
+for(var i=0; i< navManu.length;i++)
+{
+    navManu[i].addEventListener('click',function(event)
+    {
+        event.preventDefault();
+
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        var targetSection = document.getElementById(targetSectionID);
+
+        interval = setInterval(scroll,20,targetSection);
+
+    });
+}
+
+function scroll(targetSection)
+        {
+            var targetSectionCoordinates = targetSection.getBoundingClientRect();
+            if(targetSectionCoordinates.top <= 0)
+            {
+                clearInterval(interval);
+                return;
+            }
+            
+            window.scrollBy(0,50);
+            
+        }
